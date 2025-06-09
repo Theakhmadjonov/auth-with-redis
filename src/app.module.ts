19aa161ffaf006d62e63c672ec8417b473221f49
Sobirjon <ahmadjonovsobirjon24@gmail.com>
2025-06-09 21:22:51 +0500

    lk

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CoreModule],
   controllers: [],
   providers: [],
 })
